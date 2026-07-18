8018a0b02ccf8d8101e892c458d6d074319eb6e7
Akshat232007 <akshathalwai8@gmail.com>
2026-07-18 04:14:47 +0000

    Modigied app.py

--- app.py
+++ app.py
@@ -444,4 +444,4 @@ def notices():
     return render_template('notices.html', notices=n)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0",debug=True, port=5000)
